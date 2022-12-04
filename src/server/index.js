db5105bf26673ab88c94db729bae9a20c6e6fe03
Ali Alantaif <ali.entaif@gmail.com>
2022-12-04 15:24:49 +0300

    added project structure
    folder and files that we need

--- src/server/index.js
+++ src/server/index.js
@@ -7,13 +7,8 @@ projectData = {};
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 // Cors for cross origin allowance
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
 
 // Setup Server
