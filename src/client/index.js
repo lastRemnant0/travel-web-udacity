2aea0559764f2618373da9c527aba9ea82fd7184
Ali Alantaif <ali.entaif@gmail.com>
2022-12-04 18:03:09 +0300

    server config and styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,5 @@
+import './styles/reset.scss';
+import './styles/header.scss';
+import './styles/main.scss';
+import './styles/footer.scss';
 alert("I'm ALIVE");
--- src/server/index.js
+++ src/server/index.js
@@ -2,13 +2,21 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
+const cors = require('cors');
 // Start up an instance of app
-
+const app = express();
+console.log(__dirname);
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+//Here we are configuring express to use body-parser as middle-ware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
-
+app.use(express.static('./src/client/view'));
 // Setup Server
+const PORT = 8081;
+app.listen(8081, () => {
+  console.log(`listening on port: ${PORT}`);
+});
