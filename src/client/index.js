265761e333bd9289aeda95645ab8431f3036f1fe
Ali Alantaif <ali.entaif@gmail.com>
2022-12-08 17:35:13 +0300

    added page styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,5 @@
-import "./styles/reset.scss";
-import "./styles/header.scss";
-import "./styles/main.scss";
-import "./styles/footer.scss";
+import './styles/reset.scss';
+import './styles/header.scss';
+import './styles/main.scss';
+import './styles/footer.scss';
 console.log(process.env.NAME);
-window.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("U CLICKED THE WINDOW");
-});
