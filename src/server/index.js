3cd35cd4431383f732095d648101a1c0a402e4e1
Ali Alantaif <ali.entaif@gmail.com>
2022-12-14 00:52:10 +0300

    get the rest data from api

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,14 @@
+//require dotenv to be able to access enviroment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
 // Setup empty JS object to act as endpoint for all routes
 let planData;
 // Require Express to run server and routes
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 // Start up an instance of app
 const PORT = 8081;
@@ -17,32 +21,41 @@ app.use(express.json());
 // Cors for cross origin allowance
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('dist/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("dist/index.html"));
 });
 
 //post route to send the fetched data from api to client end point
-app.post('/user', async (req, res) => {
+app.post("/user", async (req, res) => {
   function getWeather(lat, log) {
     return axios.get(
-      `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${log}&key=KEY`
+      `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${log}&key=${process.env.WEATHERBIT_API_KEY}`
     );
   }
   function getCityImage(city) {
     return axios.get(
-      `https://pixabay.com/api/?key=KEY=${city}&image_type=photo&pretty=true&category=buildings`,
+      `https://pixabay.com/api/?key=${process.env.PIXBAY_API_KEY}&q=${city}&image_type=photo&pretty=true&category=buildings`,
       {
-        headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
+        headers: { "Accept-Encoding": "gzip,deflate,compress" },
       }
     );
   }
+  function getCountryImage(country) {
+    return axios.get(
+      `https://pixabay.com/api/?key=${process.env.PIXBAY_API_KEY}&q=${country}&image_type=photo&pretty=true&category=buildings`,
+      {
+        headers: { "Accept-Encoding": "gzip,deflate,compress" },
+      }
+    );
+  }
+
   try {
     const response = await axios.get(
-      'http://api.geonames.org/searchJSON?q=dammam&maxRows=1&username=KEY',
+      `http://api.geonames.org/searchJSON?q=dammam&maxRows=1&username=${process.env.GEONAMES_KEY}`,
       {
-        headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
+        headers: { "Accept-Encoding": "gzip,deflate,compress" },
       }
     );
     planData = {
@@ -51,19 +64,37 @@ app.post('/user', async (req, res) => {
       lat: response.data.geonames[0].lat,
       log: response.data.geonames[0].lng,
     };
-    res.send(planData);
   } catch (err) {
     console.log(err);
   }
   try {
     await Promise.all([
       getWeather(planData.lat, planData.log),
-      getCityImage(planData.country),
+      getCityImage(planData.city),
+      getCountryImage(planData.country),
     ]).then(function (results) {
       const weather = results[0];
-      const image = results[1];
-      console.log(`temp: ${weather.data.data[0].max_temp}
-      imageURL: ${image.data.hits[0].largeImageURL}`);
+      const imageCity = results[1];
+      const imageCountry = results[2];
+      if (imageCity.data.totalHits === 0) {
+        planData = {
+          ...planData,
+          countryImage: imageCountry.data.hits[0].largeImageURL,
+        };
+      } else {
+        planData = {
+          ...planData,
+          cityImage: imageCity.data.hits[0].largeImageURL,
+        };
+      }
+      planData = {
+        ...planData,
+        temp: weather.data.data[0].temp,
+        max_temp: weather.data.data[0].max_temp,
+        low_temp: weather.data.data[0].low_temp,
+      };
+      res.status(200).send(planData);
+      console.log(planData);
     });
   } catch (err) {
     console.log(err);
